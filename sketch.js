f94e546e19f864d8f5e7b464cc7e2da87c6c0e15
AadiGr8 <81303504+AadiGr8@users.noreply.github.com>
2021-10-28 12:27:02 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,24 +2,12 @@ var plain, plainImg;
 var city, cityImg;
 var birdImg, bird1Img, bird2Img;
 var gameState = PLAY;
-var SERVE = 0;
 var PLAY = 1;
 var END = 2;
 var edges;
-var plainG;
-var birdG, starG, star2G, diamondG;
-var starImg, star2Img;
-var score = 0;
+var bird;
 var start, startImg;
-var diamondImg;
 var gameOver, gameOverImg;
-var sky, skyImg;
-var getReady, getReadyImg;
-var ufoImg, ufoImg2;
-var ufoG, ufo2G;
-var missileImg, missileG;
-var space, spaceImg;
-var next, nextImg;
 
 function preload(){
   
@@ -27,19 +15,8 @@ function preload(){
   cityImg = loadImage("city.png");
   birdImg = loadImage("bird2.png");
   bird1Img = loadImage("bird.png");
-  bird2Img = loadImage("bird4.png");  
-  starImg = loadImage("star.png");
-  star2Img = loadImage("star2.png");
-  startImg = loadImage("start_icon.png");
-  diamondImg = loadImage("diamond.png");
+  bird2Img = loadImage("bird4.png");
   gameOverImg = loadImage("gameOver.png");
-  skyImg = loadImage("background.png");
-  getReadyImg = loadImage("getReady.png");
-  ufoImg = loadImage("ufo.png");
-  ufo2Img = loadImage("ufo2.png");
-  missileImg = loadImage("bullet.png");
-  spaceImg = loadImage("space.png");
-  nextImg = loadImage("Next.png");
   
   
 }
@@ -48,13 +25,6 @@ function setup(){
   createCanvas(800,800);  
   
   birdG = new Group();
-  starG = new Group();
-  star2G = new Group();
-  plainG = new Group();
-  diamondG = new Group();
-  ufoG = new Group();
-  ufo2G = new Group();
-  missileG = new Group(); 
 
   city = createSprite(400,400,0,0);
   city.addImage(cityImg);
@@ -75,25 +45,6 @@ function setup(){
   gameOver.visible = false;
   gameOver.scale = 0.7;
 
-  getReady = createSprite(400,325,10,10);
-  getReady.addImage(getReadyImg);
-  getReady.visible = false;
-  getReady.scale = 0.7;
-  
-  sky = createSprite(400,400,10,10);
-  sky.addImage(skyImg);
-  sky.scale = 2;
-  sky.visible = false;
-  
-  space = createSprite(380,777,10,10);
-  space.addImage(spaceImg);
-  space.scale = 0.25;
-  space.visible = false;
-  
-  next = createSprite(750,760,10,10);
-  next.addImage(nextImg);
-  next.scale = 0.2;
-  next.visible = false;
 }
 
 function draw(){
@@ -112,10 +63,8 @@ if(mousePressedOver(start)){
   if(gameState === PLAY){
   
     gameOver.visible = false;
-    city.visible = true;
     plain.visible = true;
-    sky.visible = false;
-    getReady.visible = false;
+    city.visible = true;
     start.y = 1000;
 
     plain.y = mouseY;
